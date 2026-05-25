25ab480eba48e7204ca2fe9007117eb9d702da8a
claire <claire.kimani@student.moringaschool.com>
2026-05-25 10:39:08 +0300

    testing  urls backed and frontend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -29,7 +29,7 @@ function RegisterPage() {
         }
         
     try {
-        const response = await fetch('https://hope-connect-backend-1-9syn.onrender.com/auth/register', {
+        const response = await fetch('https://connect-backend-8x61.onrender.com/auth/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
